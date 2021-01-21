687c71fac3b9b94c723b7fbb08010d35ba309429
Konstantin Grebenev <konstantin@grebenev.com>
2021-01-21 12:32:42 +0300

    feature: to hash password feature added

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose';
 
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+const scryptAsync = promisify(scrypt);
+
 interface UserAttrs {
   email: string;
   password: string;
@@ -14,6 +19,25 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
+// to hash password
+const toHashPassword = async (password: string) => {
+  const salt = randomBytes(8).toString('hex');
+  const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
+
+  return `${buffer.toString('hex')}.${salt}`;
+};
+
+// compare password
+const comparePasswords = async (
+  storedPassword: string,
+  suppliedPassword: string
+) => {
+  const [hashedPassword, salt] = storedPassword.split('.');
+  const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+  return buffer.toString('hex') === hashedPassword;
+};
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -23,6 +47,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await toHashPassword(this.get('password'));
+    this.set('password', hashed);
+  }
+
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
