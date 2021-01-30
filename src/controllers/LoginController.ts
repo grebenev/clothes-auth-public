79ff79e97283313861b0f1e429e8293a17da50ba
Konstantin Grebenev <konstantin@grebenev.com>
2021-01-30 10:36:15 +0300

    fix: request validation middleware added

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -7,6 +7,16 @@ import { classController } from '../decorators/classDecorators';
 import { User } from '../models/userModel';
 import { ValidatorErrors } from '../errors';
 
+const requestValidation = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new ValidatorErrors(errors.array());
+  }
+
+  next();
+};
+
 @classController('/api/users')
 class LoginController {
   @decorator.get('/currentuser')
@@ -17,18 +27,10 @@ class LoginController {
   @decorator.post('/signin')
   @decorator.use([
     body('email').isEmail().withMessage('Email must be valid'),
-    body('password')
-      .trim()
-      .notEmpty()
-      .withMessage('Password is required'),
+    body('password').trim().notEmpty().withMessage('Password is required'),
   ])
+  @decorator.use(requestValidation)
   signinUser(req: Request, res: Response): void {
-    const errors = validationResult(req)
-
-    if (!errors.isEmpty()) {
-      throw new ValidatorErrors(errors.array());
-    }
-
     res.json('Post request to signinUser is worked !');
   }
 
@@ -43,14 +45,8 @@ class LoginController {
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4-20 chr'),
   ])
-
+  @decorator.use(requestValidation)
   async signupUser(req: Request, res: Response) {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new ValidatorErrors(errors.array());
-    }
-
     const { email, password } = req.body;
 
     // find user email
